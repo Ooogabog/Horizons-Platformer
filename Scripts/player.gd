6e68ba30bbe416678cfb4c3dcb677f00788c17fe
Ooogabog <146573406+Ooogabog@users.noreply.github.com>
2026-07-08 00:00:23 +0500

    Collision, Platform / floor, some objects, tuned out player movement and collision
    basically build the world for the first level with static bodies

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 130.0
+const JUMP_VELOCITY = -350.0
 
 #get gravity from project settings to be synced with RigidBody Node
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,2 @@
 extends Node2D
-func _ready():
-	print("hello world")
-	print("i hate hackatime like seriously hate it")
-	print()
+	
