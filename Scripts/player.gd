a785bbfd5e17b531112f02cb3d95496cc89e90a1
Ooogabog <146573406+Ooogabog@users.noreply.github.com>
2026-07-08 05:06:11 +0500

    More animations, player directions, Custom controls and corrected gravity/ jump
    Animation added : Switching between idle and running and jump animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
-
+@onready var animated_sprite_2d: AnimatedSprite2D =  $AnimatedSprite2D
 const SPEED = 130.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -343.0
 
 #get gravity from project settings to be synced with RigidBody Node
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	#Add Animation
+	if velocity.x >1 or velocity.x< -1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+		
 	#Add gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		animated_sprite_2d.animation = "jump"
 		
 	#Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
 	#Get input direction and handle the movement/ deceleration
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x , 0 , SPEED)
+		
 	
 	move_and_slide()
+	
+	if direction== -1.0:
+		animated_sprite_2d.flip_h = true
+	else:
+		animated_sprite_2d.flip_h = false
+		
--- node_2d.gd
+++ node_2d.gd
@@ -1,2 +1,6 @@
 extends Node2D
 	
+func _readyI():
+	print(" waking up the hackatime")
+	print(" i got disconnected again ")
+	print()
