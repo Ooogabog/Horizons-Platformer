842ae17af456da752af6c33cc7312c03d07e93a3
Ooogabog <146573406+Ooogabog@users.noreply.github.com>
2026-07-13 04:19:47 +0500

    Fixed Collision! Fixed Layout
    Finally everything works smoothly !!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D =  $AnimatedSprite2D
 const SPEED = 130.0
-const JUMP_VELOCITY = -343.0
+const JUMP_VELOCITY = -550.0
 
 #get gravity from project settings to be synced with RigidBody Node
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
